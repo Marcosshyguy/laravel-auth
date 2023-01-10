74cd37eb1dea0eb1a83a26dcadd388fee22c81a4
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2023-01-10 18:56:53 +0100

    create an index page that shows all the projects

---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <table class="table table-striped table-hover">
+            <tbody>
+                @foreach ($projects as $project)
+                    <tr>
+                        <td>{{ $project->title }}</td>
+                        <td>{{ $project->production_date }}</td>
+                        <td>
+                            <a href="" class="btn btn-primary">Mostra dettagli</a>
+                            <a href="" class="btn btn-secondary">Aggiorna</a>
+                            <form action="" class="d-inline" method="POST">
+                                @method('DELETE')
+                                @csrf
+                                <button type="submit" class="btn btn-danger">Elimina</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
