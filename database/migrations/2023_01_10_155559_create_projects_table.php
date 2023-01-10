f297556543c055bc203c0b67be1203eb3e5c77ad
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2023-01-10 17:24:58 +0100

    project migration created

--- database/migrations/2023_01_10_155559_create_projects_table.php
+++ database/migrations/2023_01_10_155559_create_projects_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 50);
+            $table->text('description');
+            $table->date('production_date')->nullable();
+            $table->string('languages_used', 200);
             $table->timestamps();
         });
     }
