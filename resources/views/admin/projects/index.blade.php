1729702860efa1176416d4eb25ae650eaf5cefac
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2023-01-11 17:51:53 +0100

    add modal from  olga repository to manage the delete button

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -11,12 +11,39 @@
                         <td>
                             <a href="{{ route('admin.projects.show', $project->slug) }}" class="btn btn-primary">Mostra
                                 dettagli</a>
-                            <a href="" class="btn btn-secondary">Aggiorna</a>
-                            <form action="" class="d-inline" method="POST">
-                                @method('DELETE')
-                                @csrf
-                                <button type="submit" class="btn btn-danger">Elimina</button>
-                            </form>
+                            <a href="{{ route('admin.projects.edit', $project->slug) }}" class="btn btn-secondary">Aggiorna
+                                progetto</a>
+                            <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                                data-bs-target="#delete-post-{{ $project->id }}">
+                                <i class="fa-solid fa-trash"></i>
+                            </button>
+
+                            <!-- Modal della conferma prima della cancellazione -->
+                            <div class="modal fade" id="delete-post-{{ $project->id }}" tabindex="-1"
+                                aria-labelledby="delete-label-{{ $project->id }}" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h3 class="modal-title fs-5" id="delete-label-{{ $project->id }}">Vuoi
+                                                cancellare {{ $project->title }}?</h3>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary"
+                                                data-bs-dismiss="modal">Annulla</button>
+                                            <form action="{{ route('admin.projects.destroy', $project->slug) }}"
+                                                method="POST" class="d-inline-block">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button class="btn btn-danger" type="submit">
+                                                    Cancella
+                                                </button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </td>
                     </tr>
                 @endforeach
